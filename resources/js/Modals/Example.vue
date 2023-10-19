c215cdde4e3e573b73e4bd939e35d69e62551586
Alex Garrett-Smith <alex@codecourse.com>
2023-10-19 10:31:19 +0100

    Episode 09

--- resources/js/Modals/Example.vue
+++ resources/js/Modals/Example.vue
@@ -1,11 +1,19 @@
 <script setup>
 import Modal from '@/Components/Modals/Modal.vue'
+import { useModal } from 'momentum-modal'
+
+const { close } = useModal()
+
+const doSomething = () => {
+    // do something
+    close()
+}
 </script>
 
 <template>
     <Modal v-slot="{ close }" class="!max-w-xl">
         This is a modal
 
-        <button v-on:click="close">Close modal</button>
+        <button v-on:click="doSomething">Close modal</button>
     </Modal>
 </template>
